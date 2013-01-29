c3c5e1effc01fbc8a4bce3fc50045ebd0de68d12
nuuampkmutt <amp@webon.net>
2013-01-29 12:15:50 +0700

    Add dropmenu

--- javascripts/jquery.js
+++ javascripts/jquery.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-    $(".topMenuAction").click( function() {
-        if ($("#openCloseIdentifier").is(":hidden")) {
-            $("#slider").animate({
-                marginTop: "-141px"
-                }, 500 );
-            $("#topMenuImage").html('Log in (Open)');
-            $("#openCloseIdentifier").show();
-        } else {
-            $("#slider").animate({
-                marginTop: "0px"
-                }, 500 );
-            $("#topMenuImage").html('Log in (Close)');
-            $("#openCloseIdentifier").hide();
-        }
-    }); 
+	slidingLoginBox();
+	dropMenu();
 });
+function slidingLoginBox(){
+	$(".topMenuAction").click( function() {
+	if ($("#openCloseIdentifier").is(":hidden")) {
+		$("#slider").animate({
+			marginTop: "-141px"
+			}, 500 );
+		$("#topMenuImage").html('Log in (Open)');
+		$("#openCloseIdentifier").show();
+	} else {
+		$("#slider").animate({
+			marginTop: "0px"
+			}, 500 );
+		$("#topMenuImage").html('Log in (Close)');
+		$("#openCloseIdentifier").hide();
+	}
+	});
+}
+function dropMenu(){
+    $("#nav li").hover(
+        function () {
+            $(this).find("div.sub-nav").fadeIn("fast");
+        },
+        function () {
+            $(this).find("div.sub-nav").fadeOut("fast");
+        }
+    );
+}
